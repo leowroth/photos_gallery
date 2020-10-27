d908cfa6f1545d71b3d6341566ce826de2d2650c
leowroth <32112127+leowroth@users.noreply.github.com>
2020-10-27 11:11:16 +0100

    MainApplication now HiltAndroidApp

--- app/src/main/java/com/github/weg_li_android/MainApplication.kt
+++ app/src/main/java/com/github/weg_li_android/MainApplication.kt
@@ -1,13 +1,40 @@
 package com.github.weg_li_android
 
+import android.os.StrictMode
 import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MainApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        setupTimber()
+        setupStrictMode()
+    }
+
+    private fun setupTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun setupStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
         }
     }
 }
