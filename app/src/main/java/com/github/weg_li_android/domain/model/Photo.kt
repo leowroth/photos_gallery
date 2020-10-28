b77de27caf936b50462a0946e94b7637a9ce28ca
leowroth <32112127+leowroth@users.noreply.github.com>
2020-10-28 14:13:12 +0100

    Added serializedName to Photos.downloadUrl

--- app/src/main/java/com/github/weg_li_android/domain/model/Photo.kt
+++ app/src/main/java/com/github/weg_li_android/domain/model/Photo.kt
@@ -1,10 +1,12 @@
 package com.github.weg_li_android.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Photo(
     val id: String,
     val author: String,
     val width: Int,
     val height: Int,
     val url: String,
-    val downloadUrl: String
+    @SerializedName("download_url") val downloadUrl: String
 )
