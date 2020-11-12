2b9f8e2d4071c1c904cca76764dab0cb91e34cf2
leowroth <32112127+leowroth@users.noreply.github.com>
2020-11-12 13:34:19 +0100

    Refactored PhotosListActivity.kt

--- app/src/main/java/com/github/weg_li_android/ui/photoslist/PhotosListActivity.kt
+++ app/src/main/java/com/github/weg_li_android/ui/photoslist/PhotosListActivity.kt
@@ -2,11 +2,15 @@ package com.github.weg_li_android.ui.photoslist
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader
 import com.bumptech.glide.util.ViewPreloadSizeProvider
 import com.github.weg_li_android.GlideApp
+import com.github.weg_li_android.GlideRequest
+import com.github.weg_li_android.GlideRequests
 import com.github.weg_li_android.R
 import com.github.weg_li_android.domain.model.Photo
 import com.github.weg_li_android.ui.base.BaseActivity
@@ -16,44 +20,62 @@ import timber.log.Timber
 
 @AndroidEntryPoint
 class PhotosListActivity : BaseActivity() {
-    private lateinit var viewModel: PhotosListViewModel
-    private lateinit var adapter: PhotosListAdapter
-    private lateinit var photoList: List<Photo>
     private val amountToPreload = 10
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.photos_list)
 
-        viewModel = ViewModelProvider(this).get(PhotosListViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(PhotosListViewModel::class.java)
 
-        viewModel.photosList.observe(this, { list ->
-            val sizeProvider = ViewPreloadSizeProvider<Photo>()
-
+        viewModel.photosList.observe(this, { photosList ->
             val glideRequest = GlideApp.with(applicationContext)
             val fullRequest =
-                glideRequest.asDrawable().centerCrop().placeholder(ColorDrawable(Color.GRAY))
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    glideRequest.asDrawable().centerCrop()
+                        .placeholder(ColorDrawable(getColor(R.color.primaryColor)))
+                } else {
+                    glideRequest.asDrawable().centerCrop().placeholder(ColorDrawable(Color.MAGENTA))
+                }
 
+            val sizeProvider = ViewPreloadSizeProvider<Photo>()
 
-            adapter = PhotosListAdapter(list, sizeProvider, fullRequest)
-            adapter.setHasStableIds(true)
-            adapter.onItemClick = { position ->
-                //TODO show PhotoDetailFragment
-                Timber.d(list[position].author)
-            }
-            photosRecyclerView.adapter = adapter
-
-            val preLoader = RecyclerViewPreloader(
-                glideRequest, adapter, sizeProvider, amountToPreload
+            setupRecyclerView(
+                setupAdapter(photosList, sizeProvider, fullRequest),
+                glideRequest, sizeProvider
             )
-            photosRecyclerView.addOnScrollListener(preLoader)
-            photosRecyclerView.setItemViewCacheSize(0)
-            photosRecyclerView.setRecyclerListener { holder ->
-                glideRequest.clear(holder.itemView)
-            }
 
-            photoList = list
         })
+    }
+
+    private fun setupAdapter(
+        list: List<Photo>,
+        sizeProvider: ViewPreloadSizeProvider<Photo>,
+        fullRequest: GlideRequest<Drawable>
+    ): PhotosListAdapter {
+        val adapter = PhotosListAdapter(list, sizeProvider, fullRequest)
+        adapter.setHasStableIds(true)
+        adapter.onItemClick = { position ->
+            //TODO show PhotoDetailFragment
+            Timber.d(list[position].author)
+        }
+        return adapter
+    }
+
+    private fun setupRecyclerView(
+        adapter: PhotosListAdapter,
+        glideRequest: GlideRequests,
+        sizeProvider: ViewPreloadSizeProvider<Photo>
+    ) {
+        photosRecyclerView.adapter = adapter
+
+        val preLoader = RecyclerViewPreloader(
+            glideRequest, adapter, sizeProvider, amountToPreload
+        )
+        photosRecyclerView.addOnScrollListener(preLoader)
+        photosRecyclerView.setItemViewCacheSize(0)
+        photosRecyclerView.setRecyclerListener { holder ->
+            glideRequest.clear(holder.itemView)
+        }
     }
 }
