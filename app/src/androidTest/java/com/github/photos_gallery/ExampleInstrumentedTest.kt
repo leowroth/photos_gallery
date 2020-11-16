97a8906d9a414635cc2deb67a468ff3174fe3cc7
leowroth <32112127+leowroth@users.noreply.github.com>
2020-11-16 19:37:16 +0100

    Changed package name in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/github/photos_gallery/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/photos_gallery/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.weg_li_android", appContext.packageName)
+        assertEquals("com.github.photos_gallery", appContext.packageName)
     }
 }
