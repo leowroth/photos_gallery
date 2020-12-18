a51933ea25a47403dd848f2d3a20bf899b85b362
leowroth <32112127+leowroth@users.noreply.github.com>
2020-12-18 13:45:48 +0100

    Added default values to Photo.kt

--- app/src/main/java/com/github/leowroth/photos_gallery/domain/model/Photo.kt
+++ app/src/main/java/com/github/leowroth/photos_gallery/domain/model/Photo.kt
@@ -3,10 +3,10 @@ package com.github.leowroth.photos_gallery.domain.model
 import com.github.leowroth.photos_gallery.data.database.DatabasePhoto
 
 data class Photo(
-    val id: String,
-    val title: String,
-    val faved: Boolean,
-    val downloadUrl: String
+    val id: String = "",
+    val title: String = "",
+    val faved: Boolean = false,
+    val downloadUrl: String = ""
 )
 
 fun Photo.asDatabasePhoto(): DatabasePhoto {
