6646bb8d30530619d045e1dc8f23f21f5ee91b99
leowroth <32112127+leowroth@users.noreply.github.com>
2020-12-18 13:46:13 +0100

    Added PhotosListViewModelTest.kt with init() test

---
+++ app/src/test/java/com/github/leowroth/photos_gallery/ui/photoslist/PhotosListViewModelTest.kt
@@ -0,0 +1,67 @@
+package com.github.leowroth.photos_gallery.ui.photoslist
+
+import androidx.lifecycle.MutableLiveData
+import com.github.leowroth.photos_gallery.CoroutinesTestRule
+import com.github.leowroth.photos_gallery.domain.model.Photo
+import com.github.leowroth.photos_gallery.domain.usecase.GetPhotosUseCase
+import com.github.leowroth.photos_gallery.domain.usecase.PhotoFavedUseCase
+import com.github.leowroth.photos_gallery.domain.usecase.RefreshPhotosUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class PhotosListViewModelTest {
+
+    @get:Rule
+    var coroutinesTestRule = CoroutinesTestRule()
+
+    private val getPhotosUseCase = mockk<GetPhotosUseCase>()
+
+    private val refreshPhotosUseCase =
+        mockk<RefreshPhotosUseCase>()
+
+    private val photoFavedUseCase = mockk<PhotoFavedUseCase>()
+
+    private lateinit var photosListViewModel: PhotosListViewModel
+
+
+    @Before
+    fun setup() {
+        coEvery {
+            photoFavedUseCase.photoFaved(any())
+        } answers { nothing }
+        coEvery {
+            refreshPhotosUseCase.invoke()
+        } answers { nothing }
+    }
+
+
+    @Test
+    fun `viewModel calls refreshPhotosUseCase at initiation`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            val expectedPhotos = MutableLiveData(
+                listOf(
+                    Photo(id = "First"), Photo(id = "Second")
+                )
+            )
+            coEvery { getPhotosUseCase.invoke() } answers { expectedPhotos }
+
+            photosListViewModel =
+                PhotosListViewModel(
+                    getPhotosUseCase,
+                    refreshPhotosUseCase,
+                    photoFavedUseCase
+                )
+
+            assertEquals(false, photosListViewModel.eventNetworkError.value)
+            assertEquals(expectedPhotos, photosListViewModel.photosList)
+            coVerify { refreshPhotosUseCase.invoke() }
+        }
+}
